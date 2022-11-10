5d7c5c14f12dc3c6901bd5132bd6ea0566f22c42
YounnesYBN <younnesYBN@outlook.com>
2022-11-10 21:13:49 +0100

    finishing the data controling and displaying message with the popup we created

--- view/login fornisseur/fornisseur_login.js
+++ view/login fornisseur/fornisseur_login.js
@@ -1,5 +1,5 @@
 $(document).ready(()=>{
-    
+    //for the popup
     $("#popup_child").click(function(e){
         e.stopPropagation()
         
@@ -9,9 +9,69 @@ $(document).ready(()=>{
         $("#popup").fadeOut(500); 
     });
     function ActivePopUp(message){
-        $("#message_pop").innerText(message)
+        console.log(document.getElementById("message_pop"))
+        document.getElementById("message_pop").innerHTML= message
         $("#popup").fadeIn(500); 
     }
+    //for the popup
+
+    var emailValid = true;
+    var passwordValid = true;
+
+    //function to check email
+    function onEmailChange(){
+        const emailEle = document.getElementById("email")
+        const emailVal = emailEle.value
+        const Emlrgx = /^[a-zA-Z]{1,}@[a-zA-Z]{1,}\.[a-zA-Z]{1,}$/
+        const test = Emlrgx.test(emailVal)
+        if(test==true ){
+            emailEle.style.borderColor = "#9b59b6";
+            emailValid = true;
+        }else{
+            emailEle.style.borderColor = "red";
+            emailValid = false;
+        }
+    }
+    //function to check password
+    function onPasswordChange(){
+        const passWordEle = document.getElementById("password");
+        const passWordVal = passWordEle.value;
+        const passWordlen = passWordVal.length
+        if(passWordlen>0){
+            passWordEle.style.borderColor = "#9b59b6";
+            passwordValid = true;
+
+        }else{
+            passWordEle.style.borderColor = "red";
+            passwordValid = false;
+        }
+    }
+
+    $("#email").keyup(function(e){
+        onEmailChange()
+    })
+
+    $("#password").keyup(function (e) { 
+        onPasswordChange()
+    });
+
+    $("#logInForm").submit(function(e){
+        onEmailChange();
+        onPasswordChange();
+        if(emailValid==true & passwordValid==true){
+            //here where we going to send data to backend which is controler
+        }else{
+            var message = emailValid==false&passwordValid==false?"-Email is unvalide Exemple: 'username@gmail.com' <br> -passwored is unvalide .":emailValid==false?"-Email is unvalide Exemple: 'username@gmail.com'":"-passwored is unvalide ." ;
+            ActivePopUp(message)
+            e.preventDefault() 
+        }
+    })
+
+    $("#logInButton").click(function(){
+        onEmailChange();
+        onPasswordChange();
+    })
+    
     
 })
      
