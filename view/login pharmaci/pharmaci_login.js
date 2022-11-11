fbdd0c6739f1de74c094d4dba37160421dd607c7
YounnesYBN <younnesYBN@outlook.com>
2022-11-11 19:33:10 +0100

    finishing the js part and controling data for login pharmaci

--- view/login pharmaci/pharmaci_login.js
+++ view/login pharmaci/pharmaci_login.js
@@ -15,7 +15,63 @@ $(document).ready(()=>{
     }
     //for the popup
 
-    
+    var emailValid = true;
+    var passwordValid = true;
+
+    //function to check email
+    function onEmailChange(){
+        const emailEle = document.getElementById("email")
+        const emailVal = emailEle.value
+        const Emlrgx = /^[a-zA-Z0-9._-]{1,}@[a-zA-Z0-9._-]{1,}\.[a-z]{3,10}$/
+        const test = Emlrgx.test(emailVal)
+        if(test==true ){
+            emailEle.style.borderColor = "#9b59b6";
+            emailValid = true;
+        }else{
+            emailEle.style.borderColor = "red";
+            emailValid = false;
+        }
+    }
+    //function to check password
+    function onPasswordChange(){
+        const passWordEle = document.getElementById("password");
+        const passWordVal = passWordEle.value;
+        const passWordlen = passWordVal.length
+        if(passWordlen>=10){
+            passWordEle.style.borderColor = "#9b59b6";
+            passwordValid = true;
+
+        }else{
+            passWordEle.style.borderColor = "red";
+            passwordValid = false;
+        }
+    }
+
+    $("#email").keyup(function(e){
+        onEmailChange()
+    })
+
+    $("#password").keyup(function (e) { 
+        onPasswordChange()
+    });
+
+    $("#logForm").submit(function(e){
+        onEmailChange();
+        onPasswordChange();
+        if(emailValid==true & passwordValid==true){
+        
+        }else{
+            var message = emailValid==false&passwordValid==false?"-Email is unvalide Exemple: 'username@gmail.com' <br> -passwored is unvalide .":emailValid==false?"-Email is unvalide Exemple: 'username@gmail.com'":"-passwored is unvalide ." ;
+            ActivePopUp(message)
+            e.preventDefault() 
+        }
+    })
+
+    $("#logbutton").click(function(){
+        onEmailChange();
+        onPasswordChange();
+    })
+
     
 })
      
