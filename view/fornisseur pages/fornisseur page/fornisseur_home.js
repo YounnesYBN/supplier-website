870cc8de6d75cb2ffe4c55d78a34bb80e01c2664
YounnesYBN <younnesYBN@outlook.com>
2022-11-21 15:56:54 +0100

    finishing controlling data for delete and edite popup

--- view/fornisseur pages/fornisseur page/fornisseur_home.js
+++ view/fornisseur pages/fornisseur page/fornisseur_home.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
         const namrgx =/^[a-zA-Z]{1,}$/
         const test = namrgx.test(nameVal)
         if(test==true ){
-            nameEle.style.borderColor = "#9b59b6";
+            nameEle.style.borderColor = "#00d4ff";
             nameValidAdd = true;
             addErrorObject.nameErr=""
         }else{
@@ -104,7 +104,7 @@ $(document).ready(function(){
         const nbrrgx= /^[0-9]{1,}$/
         const test = nbrrgx.test(numberVal)
         if(test == true){
-            numberEle.style.borderColor = "#9b59b6";
+            numberEle.style.borderColor = "#00d4ff";
             numberValidAdd = true;
             addErrorObject.numberErr=""
 
@@ -121,7 +121,7 @@ $(document).ready(function(){
         const  pricrgx= /^[0-9]{1,}$/
         const test = pricrgx.test(priceVal)
         if(test == true){
-            priceEle.style.borderColor = "#9b59b6";
+            priceEle.style.borderColor = "#00d4ff";
             priceValidAdd = true;
             addErrorObject.priceErr="";
 
@@ -167,6 +167,139 @@ $(document).ready(function(){
         }
     })
     // Anas code
+    //code to controle delete popup
+        selectValidDelete = true;
+        function onSelectDeleteChange(){
+            const selectDeleteElement = document.getElementById("delete_select")
+            const selectDeleteval = selectDeleteElement.value
+            if(selectDeleteval=="default"){
+                selectDeleteElement.style.borderColor = "red";
+                selectValidDelete = false;
+
+            }else{
+                selectDeleteElement.style.borderColor = "#00d4ff";
+                selectValidDelete = true;
+            }
+        }
+        $("#delete_select").change((e)=>{
+            onSelectDeleteChange()
+        })
+        $("#delete").click((e)=>{
+            e.stopPropagation()
+            onSelectDeleteChange()
+
+            if(selectValidDelete){
+                ActiveSuccessPopUp("-deleted successully")
+            }else{
+                ActiveErrorPopUp("-you need to choose a medicent")
+            }
+        })
+        
+
+    //code to controle delete popup
+    var editeErrorObject = {nameErr : "",numberErr:"",priceErr:"",editeSelect:""}
+    var selectValidEdite = true;
+    var nameValidEdite = true;
+    var numberValidEdite = true;
+    var priceValidEdite = true;
+    function onNameChangeEdite(){
+        const nameEle = document.getElementById("medicient_name_edite")
+        const nameVal = nameEle.value
+        const namrgx =/^[a-zA-Z]{1,}$/
+        const test = namrgx.test(nameVal)
+        if(test==true ){
+            nameEle.style.borderColor = "#00d4ff";
+            nameValidEdite = true;
+            editeErrorObject.nameErr=""
+        }else{
+            nameEle.style.borderColor = "red";
+            nameValidEdite = false;
+            editeErrorObject.nameErr="-name is unvalid it should be string"
+
+        }
+    }
+    
+    function onNumberChangeEdite(){
+        const numberEle = document.getElementById("number_of_medicents_edite");
+        const numberVal = numberEle.value;
+        const nbrrgx= /^[0-9]{1,}$/
+        const test = nbrrgx.test(numberVal)
+        if(test == true){
+            numberEle.style.borderColor = "#00d4ff";
+            numberValidEdite  = true;
+            editeErrorObject.numberErr=""
+
+        }else{
+            numberEle.style.borderColor = "red";
+            numberValidEdite  = false;
+            editeErrorObject.numberErr="-number is unvalid it should be int";
+        }
+    }
+    
+    function onPriceChangeEdite(){
+        const priceEle = document.getElementById("price_edite");
+        const priceVal = priceEle.value;
+        const  pricrgx= /^[0-9]{1,}$/
+        const test = pricrgx.test(priceVal)
+        if(test == true){
+            priceEle.style.borderColor = "#00d4ff";
+            priceValidEdite = true;
+            editeErrorObject.priceErr="";
+
+        }else{
+            priceEle.style.borderColor = "red";
+            priceValidEdite = false;
+            editeErrorObject.priceErr="-price is unvalid it should be int";
+            
+        }
+    }
+    $("#medicient_name_edite").keyup(()=>{
+        onNameChangeEdite()
+    })
+    $("#number_of_medicents_edite").keyup(()=>{
+        onNumberChangeEdite()
+    })
+    $("#price_edite").keyup(()=>{
+        onPriceChangeEdite()
+    })
+
+
+    function onSelectEditeChange(){
+        const selectDeleteElement = document.getElementById("edite_select")
+        const selectDeleteval = selectDeleteElement.value
+        if(selectDeleteval=="default"){
+            selectDeleteElement.style.borderColor = "red";
+            selectValidEdite = false;
+            editeErrorObject.editeSelect = "-you need  to select a medicent"
+
+        }else{
+            selectDeleteElement.style.borderColor = "#00d4ff";
+            selectValidEdite = true;
+            editeErrorObject.editeSelect = ""
+        }
+    }
+    $("#edite_select").change(()=>{
+        onSelectEditeChange()
+    })
+    $("#edite").click((e)=>{
+        e.stopPropagation()
+        onNameChangeEdite()
+        onNumberChangeEdite()
+        onPriceChangeEdite()
+        onSelectEditeChange()
+        if(nameValidEdite==true&numberValidEdite==true&priceValidEdite==true&selectValidEdite==true){
+            ActiveSuccessPopUp("inputs are valide")
+        }else{
+            var message = Object.values(editeErrorObject).filter((value)=>{
+                if(value.length>0){
+                    return value
+                }
+           }).join("<br>")
+           ActiveErrorPopUp(message)
+        }
+    })
+    
+    //code to controle edite popup
 
     //exite button
     $("#logout").click(()=>{
