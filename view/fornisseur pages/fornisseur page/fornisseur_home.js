6d69010147a8a1fc89e4a08ca5c828133f76f914
YounnesYBN <younnesYBN@outlook.com>
2022-11-21 14:06:58 +0100

    controling add info

--- view/fornisseur pages/fornisseur page/fornisseur_home.js
+++ view/fornisseur pages/fornisseur page/fornisseur_home.js
@@ -74,6 +74,100 @@ $(document).ready(function(){
     }
     //succes error popup
 
+    //anas code
+    var addErrorObject = {nameErr : "",numberErr:"",priceErr:""}
+    var nameValidAdd = true;
+    var numberValidAdd = true;
+    var priceValidAdd = true;
+
+    //function to check name
+    function onNameChange(){
+        const nameEle = document.getElementById("medicient_name")
+        const nameVal = nameEle.value
+        const namrgx =/^[a-zA-Z]{1,}$/
+        const test = namrgx.test(nameVal)
+        if(test==true ){
+            nameEle.style.borderColor = "#9b59b6";
+            nameValidAdd = true;
+            addErrorObject.nameErr=""
+        }else{
+            nameEle.style.borderColor = "red";
+            nameValidAdd = false;
+            addErrorObject.nameErr="-name is unvalid it should be string"
+
+        }
+    }
+    //function to check number
+    function onNumberChange(){
+        const numberEle = document.getElementById("number_of_medicents");
+        const numberVal = numberEle.value;
+        const nbrrgx= /^[0-9]{1,}$/
+        const test = nbrrgx.test(numberVal)
+        if(test == true){
+            numberEle.style.borderColor = "#9b59b6";
+            numberValidAdd = true;
+            addErrorObject.numberErr=""
+
+        }else{
+            numberEle.style.borderColor = "red";
+            numberValidAdd = false;
+            addErrorObject.numberErr="-number is unvalid it should be int";
+        }
+    }
+    //function to check price
+    function onPriceChange(){
+        const priceEle = document.getElementById("price");
+        const priceVal = priceEle.value;
+        const  pricrgx= /^[0-9]{1,}$/
+        const test = pricrgx.test(priceVal)
+        if(test == true){
+            priceEle.style.borderColor = "#9b59b6";
+            priceValidAdd = true;
+            addErrorObject.priceErr="";
+
+        }else{
+            priceEle.style.borderColor = "red";
+            priceValidAdd = false;
+            addErrorObject.priceErr="-price is unvalid it should be int";
+            
+        }
+    }
+
+    $("#medicient_name").keyup(function(e){
+        onNameChange()
+    })
+
+    $("#number_of_medicents").keyup(function (e) { 
+        onNumberChange()
+    });
+
+    $("#price").keyup(function (e) { 
+        onPriceChange()
+    });
+
+    $("#add").click(function(e){
+        e.stopPropagation()
+
+        onNameChange();
+        onNumberChange();
+        onPriceChange()
+        if(nameValidAdd==true & numberValidAdd==true & priceValidAdd==true){
+            var name = document.getElementById("medicient_name").value
+            var number = document.getElementById("number_of_medicents").value
+            var price = document.getElementById("price").value
+
+            ActiveSuccessPopUp("input are valide")
+        }else{
+           var message = Object.values(addErrorObject).filter((value)=>{
+                if(value.length>0){
+                    return value
+                }
+           }).join("<br>")
+           ActiveErrorPopUp(message)
+        }
+    })
+    // Anas code
+
     //exite button
     $("#logout").click(()=>{
         $.ajax({
